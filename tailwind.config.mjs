7eedf87d4c21f74138d87c03b6c70d657181afe7
Ariel Heredia <arielarmandoheredia@gmail.com>
2025-07-05 22:04:17 -0400

    day/night

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,10 @@
+// tailwind.config.mjs
+
 /** @type {import('tailwindcss').Config} */
 export default {
+    // THIS LINE IS ESSENTIAL
+    darkMode: 'class',
+    
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
       extend: {},
